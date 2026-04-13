19be861a118df0ea46123ef7078ab599766b99f7
radhikasaini24 <radhikasaini726@gmail.com>
2026-04-14 04:32:30 +0530

    Create letter_template.py

---
+++ letter_template.py
@@ -0,0 +1,7 @@
+letter = '''
+          Dear<|Name|>,
+          you are selected!
+          <|Date|>
+           '''
+print(letter.replace("<|Name|>", "Radhika").replace("<|Date|","14 april 2026"))
+                  
