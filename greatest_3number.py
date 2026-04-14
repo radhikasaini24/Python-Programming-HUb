95c02e494687084739c346f72c61c3ab870828f7
radhikasaini24 <radhikasaini726@gmail.com>
2026-04-15 02:41:12 +0530

    Create greatest_3number.py

---
+++ greatest_3number.py
@@ -0,0 +1,11 @@
+a = 1
+b = 2
+c = 3
+def greatest (a, b, c):
+  if (a > b and a > c):
+    return a
+  elif (b > a and b > c)
+    return b
+  elif (c > a nd c > b)
+     return c
+print(greatest(a, b, c))
