85040f336e94277925e34217492c4f5aecac3ff7
radhikasaini24 <radhikasaini726@gmail.com>
2026-04-15 03:33:43 +0530

    Create username.py

---
+++ username.py
@@ -0,0 +1,7 @@
+#to find whether given username contains less than 10 characters or not.
+username = input("username: ")
+if (len(username)<10):
+    print("your username conatins less than 10 characters")
+else:
+    print("all is well!")
+    
